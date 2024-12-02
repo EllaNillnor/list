20b8afa86de4fd7cedd9e86789b6e9c8d63da1f3
EllaNillnor <elni06001@utb.vaxjo.se>
2024-12-02 10:39:23 +0100

    list

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+List<double> flyttalslista = new List<double> ();
+flyttalslista.Add(3.2);
+flyttalslista.Add(4.2);
+flyttalslista.Add(1.2);
+flyttalslista.Add(5.2);
+flyttalslista.Add(2.2);
+flyttalslista.RemoveAt(3);
+
+foreach(double element in flyttalslista)
+{
+    Console.WriteLine(element); 
+}
+
+
+{
+  
+    double summa = flyttalslista[0]+flyttalslista[1]+flyttalslista[2]+flyttalslista[3];
+// hur skriver man utrycket för summan enklare??
+    Console.Write("Medelvärdet är: ");
+    Console.WriteLine(Medelvärde(summa)); 
+}
+
+ double Medelvärde(double s)
+{
+    double M = s/flyttalslista.Count ;
+    return M;
+}
